5fac5474c23d2b86f04f59f3c63535557e5b3b04
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-06-26 16:01:41 +0300

    add

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,7 @@ import { RiPhoneFill } from "react-icons/ri";
 import { RiUserFill } from "react-icons/ri";
 import { ModalWindow } from "../modalWindow/ModalWindow";
 import { useState } from "react";
-
+import toast from "react-hot-toast";
 const Contact = ({ data: { name, number, id } }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [isEditingName, setIsEditingName] = useState(false);
@@ -42,7 +42,7 @@ const Contact = ({ data: { name, number, id } }) => {
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       if (isEditingName) {
         handleSaveName();
       } else if (isEditingNumber) {
@@ -51,68 +51,75 @@ const Contact = ({ data: { name, number, id } }) => {
     }
   };
 
+  return (
+    <div className={css.container}>
+      <div className={css.card}>
+        <p className={css.name} onClick={handleEditNameClick}>
+          {isEditingName ? (
+            <>
+              <TextField
+                value={editedName}
+                onChange={handleNameChange}
+                className={css.textField}
+                onKeyDown={handleKeyDown}
+              />
+            </>
+          ) : (
+            <>
+              <RiUserFill size="25" className={css.icon} />
+              {editedName}
+            </>
+          )}
+        </p>
+        <p className={css.number} onClick={handleEditNumberClick}>
+          {isEditingNumber ? (
+            <>
+              <TextField
+                value={editedNumber}
+                onChange={handleNumberChange}
+                className={css.textField}
+                onKeyDown={handleKeyDown}
+                sx={{ backgroundColor: "#deedf6" }}
+              />
+            </>
+          ) : (
+            <>
+              <RiPhoneFill size="25" className={css.icon} />
+              {editedNumber}
+            </>
+          )}
+        </p>
+      </div>
 
-return (
-  <div className={css.container}>
-    <div className={css.card}>
-      <p className={css.name} onClick={handleEditNameClick}>
-        {isEditingName ? (
-          <>
-            <TextField 
-              value={editedName}
-              onChange={handleNameChange}
-              className={css.textField}
-              onKeyDown={handleKeyDown}
-             
-            />
-          </>
-        ) : (
-          <>
-            <RiUserFill size="25" className={css.icon} />
-            {editedName}
-          </>
-        )}
-      </p>
-      <p className={css.number} onClick={handleEditNumberClick}>
-        {isEditingNumber ? (
-          <>
-            <TextField 
-              value={editedNumber}
-              onChange={handleNumberChange}
-              className={css.textField}
-              onKeyDown={handleKeyDown} 
-              sx={{ backgroundColor: "#deedf6" }}
-            />
-          </>
-        ) : (
-          <>
-            <RiPhoneFill size="25" className={css.icon} />
-            {editedNumber}
-          </>
-        )}
-      </p>
-    </div>
-
-    <Button
-      variant="contained"
-      type="submit"
-      sx={{ backgroundColor: "#1179f0", margin: "auto" }}
-      onClick={() => setModalOpen(true)}
-    >
-      Delete
-    </Button>
-    {modalOpen && (
-      <ModalWindow
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-        id={id}
-        handleDelete={() => dispatch(deleteContact(id))}
+      <Button
+        variant="contained"
+        type="submit"
+        sx={{ backgroundColor: "#1179f0", margin: "auto" }}
+        onClick={() => setModalOpen(true)}
       >
-        <p>Confirm the deletion of the contact</p>
-      </ModalWindow>
-    )}
-  </div>
-);
+        Delete
+      </Button>
+      {modalOpen && (
+        <ModalWindow
+          isOpen={modalOpen}
+          onClose={() => setModalOpen(false)}
+          id={id}
+          handleDelete={() =>
+            dispatch(deleteContact(id))
+              .unwrap()
+              .then(() => {
+                toast.success("Сontact delete successfully");
+              })
+              .catch(() => {
+                toast.error("Error delete contact");
+              })
+          }
+        >
+          <p>Confirm the deletion of the contact</p>
+        </ModalWindow>
+      )}
+    </div>
+  );
 };
 
 export default Contact;
